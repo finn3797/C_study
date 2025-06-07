a076aed15c3e89bd6bbcb5c22e79b879c7b46d09
Yuwei_Gou <379723586@qq.com>
2025-06-07 20:54:54 +0800

    temp

---
+++ code/class19_object/object_readme.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: Yuwei_Gou 379723586@qq.com
+ * @Date: 2025-05-04 19:35:50
+ * @LastEditors: Yuwei_Gou 379723586@qq.com
+ * @LastEditTime: 2025-05-20 08:50:15
+ * @FilePath: /C_study/code/class19_object/object_readme.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+#include <iostream>
+
+
+
+int main(){
+    
+}
