acc249d2765fc9826e1af1b39ff47a1c7d71a5aa
JAMBO <379723586@qq.com>
2025-06-16 17:14:11 +0800

    高精度大数加法减法的实现_减法one wa

---
+++ code/class19_object/P1601.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <string>
+#include <ctype.h>
+#include <vector>
+
+int max(int a, int b) { return a > b ? a : b; }
+
+class BigInt
+{
+    int *digits = nullptr; // 数字数组，存储数字的每一位
+    int size = 0;          // 数字数组的大小
+public:
+    // 构造函数，用于初始化对象的状态
+    BigInt(const BigInt &other)
+    {
+        size = other.size;
+        digits = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            digits[i] = other.digits[i];
+        }
+    }
+    BigInt(const std::string &str) : size(str.length())
+    {
+        digits = new int[size];
+        for (int i = 0; i < size; i++)
+            digits[i] = int(str[i] - '0');
+    }
+
+    BigInt(const std::vector<int> &vec) : size(vec.size())
+    {
+        // std::cout << "size: " << size << std::endl;
+        digits = new int[size];
+        for (int i = 0; i < size; i++)
+        {
+            digits[i] = vec[i];
+        }
+    }
+
+    BigInt &operator+=(const BigInt &other)
+    {                                             // 重载 += 运算符，用于将两个大整数相加，并将结果存储在当前对象中
+        int max_size = max(size, other.size) + 1; // 找到两个大整数中较大的大小
+        int lhd_diff = max_size - size;           // 计算左操作数的位数差
+        int rhd_diff = max_size - other.size;     // 计算右操作数的位数差
+        std::string res(max_size, '0');
+        int carry = 0;
+        for (int i = max_size - 1; i >= 0; i--)
+        {
+            int lhd = 0, rhd = 0;
+            if (i - lhd_diff >= 0)
+                lhd = digits[i - lhd_diff];
+            if (i - rhd_diff >= 0)
+                rhd = other.digits[i - rhd_diff];
+            int temp = (int)lhd + (int)rhd + carry;
+            // std::cout << lhd << " " << rhd << " " << temp << std::endl;
+            carry = temp / 10;
+            res[i] = (temp % 10) + '0';
+        }
+
+        delete[] digits;
+        if (res.front() == '0')
+            res.erase(res.begin());
+
+        // 用新结果更新当前对象
+        size = res.length();
+        digits = new int[size];
+        for (short i = 0; i < size; i++)
+        {
+            digits[i] = res[i] - '0';
+        }
+        return *this;
+    }
+
+    BigInt operator*(const BigInt &other)
+    {
+        if ((size == 1 && digits[0] == 0) || (other.size == 1 && other.digits[0] == 0))
+            return BigInt("0");
+        int res_size = size + other.size;
+        // std::cout << "res_size: " << res_size << std::endl;
+        std::vector<int> res(res_size, 0);
+        std::vector<std::vector<int>> per_quan_vec(other.size, std::vector<int>(res_size, 0));
+        for (int i = other.size - 1, j = 0; i >= 0; i--, j++)
+        {
+            int index = j;
+            int carry = 0;
+            for (int k = size - 1; k >= 0; k--)
+            {
+                int temp_num = other.digits[i] * digits[k] + carry;
+                per_quan_vec[j][res_size - 1 - index] = temp_num % 10;
+                carry = temp_num / 10;
+                index++;
+            }
+        }
+
+        int carry = 0;
+        for (auto &vec : per_quan_vec)
+        {
+            for (int i = res_size - 1; i >= 0; i--)
+            {
+                // std::cout << vec[i] << " ";
+                int temp = (res[i] + vec[i] + carry);
+                res[i] = temp % 10;
+                carry = temp / 10;
+            }
+        }
+        if (res.front() == 0)
+            res.erase(res.begin());
+
+        BigInt result = BigInt(res);
+        return result;
+    }
+    // 析构函数，用于释放对象的资源
+
+    ~BigInt()
+    {
+        if (digits == nullptr)
+            return;
+        delete[] digits;
+        digits = nullptr;
+    }
+
+    friend void printBigInt(const BigInt &bigint);
+};
+
+void printBigInt(const BigInt &bigint)
+{
+    for (int i = 0; i < bigint.size; i++)
+    {
+        std::cout << bigint.digits[i];
+    }
+    std::cout << std::endl;
+}
+int main()
+{
+    std::string a, b;
+    std::cin >> a >> b;
+    BigInt c(a);
+    BigInt d(b);
+    c += d;
+    printBigInt(c);
+}
--- code/class19_object/P2142.cpp
+++ code/class19_object/P2142.cpp
@@ -1,1 +1,123 @@
+#include <string>
+#include <iostream>
+#include <vector>
 
+
+bool max(int* digits1, int size1, int* digits2, int size2){
+    if (size1 < size2)
+        return false;
+    else if(size1 > size2)
+        return true;
+    else{
+        for (int i = 0; i < size1; i++){
+            // std::cout << digits1[i] << " : " << digits2[i] << std::endl;
+            if (digits1[i] < digits2[i])
+                return false;
+        }
+    }
+    return true;
+}
+
+
+class BigInt{
+    bool sign = true;
+    int size = 0;
+    int* digits = nullptr;
+    public:
+        BigInt(const std::string &str){
+            int start = 0;
+            if (str.front() == '-'){
+                sign = false;
+                start = 1;
+                size = str.length() - 1;
+            }
+            else{
+                sign = true;
+                size = str.length();
+            }
+            // std::cout << "开始构造，size为:" << size << std::endl;
+            digits = new int[size];
+            int str_length = str.length();
+            for (int i = start, j = 0; i < str_length; i++, j++){
+                digits[j] = int(str[i] - '0');
+            }
+        }
+
+        BigInt(std::vector<int> &vec, bool sign): sign(sign){
+            int bool_start = 0;
+            for (auto x: vec){
+                if (x != 0 || vec.size() == 1)
+                    break;
+                bool_start++;
+            }
+            size = vec.size() - bool_start;
+            // std::cout << "res_size: " << size << std::endl;
+            digits = new int[size];
+            for (int i = 0; i < size; i++){
+                digits[i] = vec[i+bool_start];
+            }
+        }
+
+        BigInt operator-(BigInt &other){
+            bool res_sign = max(digits, size, other.digits, other.size);
+            // std::cout << "res_sign: " << res_sign << std::endl;
+            int max_size = other.size > size ? other.size : size;
+            // std::cout << "max_size: " << max_size << std::endl;
+            std::vector<int> temp_digit(max_size, 0);
+            int diff_lhs = max_size - size;
+            int diff_rhs = max_size - other.size;
+            int carry = 0;
+            for (int i = max_size - 1; i >= 0; i--){
+                int lhsindex = i - diff_lhs;
+                int rhsindex = i - diff_rhs;
+                int lhsnum = lhsindex < 0 ? 0 : digits[lhsindex];
+                int rhsnum = rhsindex < 0 ? 0 : other.digits[rhsindex];
+
+                // std::cout << lhsnum << ":: rhsnum: "<< rhsnum << std::endl;
+                int real_lhsnum, real_rhsnum;
+                if (res_sign){
+                    real_lhsnum = lhsnum;
+                    real_rhsnum = rhsnum;
+                }else{
+                    real_lhsnum = rhsnum;
+                    real_rhsnum = lhsnum;
+                }
+                real_lhsnum = real_lhsnum - carry;
+                carry = real_lhsnum >= real_rhsnum ? 0 : 1;
+                if (real_lhsnum >= real_rhsnum){
+                    carry = 0;
+                    temp_digit[i] = real_lhsnum - real_rhsnum;
+                    // std::cout << "index i:" << i<< "  num: " << real_lhsnum - real_rhsnum<<std::endl;
+                }else{
+                    temp_digit[i] = 10 + real_lhsnum - real_rhsnum;
+                    // std::cout << "index i:" << i<< "  num: " << 10 + real_lhsnum - real_rhsnum<<std::endl;
+                    carry = 1;
+                }
+            }
+            // std::cout << "temp_digit_size: " << temp_digit.size() << std::endl;
+            BigInt result(temp_digit, res_sign);
+            return result;
+        }
+        ~BigInt(){
+            if (digits == nullptr) return;
+            delete[] digits;
+            digits = nullptr;
+        }
+    friend void printBigInt(const BigInt &b);
+};
+
+
+void printBigInt(const BigInt &b){
+    // std::cout << b.sign << " ::: " << b.size <<  " " << b.digits[0] <<std::endl;
+    if (!b.sign) std::cout << "-";
+    for (int i = 0; i < b.size; i++)
+        std::cout << b.digits[i];
+}
+
+
+int main(){
+    std::string str1, str2;
+    std::cin >> str1 >> str2;
+    BigInt b1(str1), b2(str2);
+    printBigInt(b1 - b2);
+}
